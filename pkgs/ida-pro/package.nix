b7de8e7f1f8a20ca8c3f3b1fdbea360e983b4cf1
miruku <miruku@miruku.moe>
2025-04-12 10:15:00 +0800

    fix

---
+++ pkgs/ida-pro/package.nix
@@ -0,0 +1,153 @@
+{
+  autoPatchelfHook,
+  cairo,
+  copyDesktopItems,
+  dbus,
+  requireFile,
+  fontconfig,
+  freetype,
+  glib,
+  gtk3,
+  lib,
+  libdrm,
+  libGL,
+  libkrb5,
+  libsecret,
+  libsForQt5,
+  libunwind,
+  libxkbcommon,
+  makeWrapper,
+  openssl,
+  python3,
+  stdenv,
+  xorg,
+  zlib,
+}:
+
+let
+  srcs = builtins.fromJSON (builtins.readFile ./srcs.json);
+in
+stdenv.mkDerivation rec {
+  pname = "ida-pro";
+  version = "9.1.0.250226";
+
+  # https://auth.lol/ida
+  src = requireFile {
+    name = "ida-pro_91_x64linux.run";
+    url = "magnet:?xt=urn:btih:920c1a578e815e9d0e4b843179306cdcb5e8e00d&dn=idapro90rc1";
+    hash = "sha256-j/CAIr46DvaTqePqAQENE1aybP3Lvn/daNAbPJcA+eI=";
+  };
+
+  patcher = ./keygen3.py;
+
+  nativeBuildInputs = [
+    makeWrapper
+    copyDesktopItems
+    autoPatchelfHook
+    libsForQt5.wrapQtAppsHook
+    python3
+  ];
+
+  # We just get a runfile in $src, so no need to unpack it.
+  dontUnpack = true;
+
+  # Add everything to the RPATH, in case IDA decides to dlopen things.
+  runtimeDependencies = [
+    cairo
+    dbus
+    fontconfig
+    freetype
+    glib
+    gtk3
+    libdrm
+    libGL
+    libkrb5
+    libsecret
+    libsForQt5.qtbase
+    libsForQt5.qtwayland
+    libunwind
+    libxkbcommon
+    openssl
+    stdenv.cc.cc
+    xorg.libICE
+    xorg.libSM
+    xorg.libX11
+    xorg.libXau
+    xorg.libxcb
+    xorg.libXext
+    xorg.libXi
+    xorg.libXrender
+    xorg.xcbutilimage
+    xorg.xcbutilkeysyms
+    xorg.xcbutilrenderutil
+    xorg.xcbutilwm
+    zlib
+  ];
+  buildInputs = runtimeDependencies;
+
+  dontWrapQtApps = true;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin $out/lib $out/opt $out/share
+
+    # IDA depends on quite some things extracted by the runfile, so first extract everything
+    # into $out/opt, then remove the unnecessary files and directories.
+    IDADIR=$out/opt
+
+    # where IDA will save its .desktop entries
+    HOME=$out/share
+    mkdir -p $out/share/.local/share/applications
+
+    # Invoke the installer with the dynamic loader directly, avoiding the need
+    # to copy it to fix permissions and patch the executable.
+    $(cat $NIX_CC/nix-support/dynamic-linker) $src \
+      --mode unattended --prefix $IDADIR
+
+    # move desktop entries to where they belongs
+    mv $out/share/.local/share/applications $out/share/applications
+    rm -r $out/share/.local
+
+    patch libida.so
+    pushd $IDADIR
+    python $patcher
+    mv libida.so.patched libida.so
+    mv libida32.so.patched libida32.so
+    popd
+
+    # move IDA remote debug servers
+    mv $IDADIR/dbgsrv $out/share
+
+    # Some libraries come with the installer.
+    addAutoPatchelfSearchPath $IDADIR
+
+    # wrap executables
+    for bb in ida assistant hvui; do
+      wrapProgram $IDADIR/$bb \
+        --prefix QT_PLUGIN_PATH : $IDADIR/plugins/platforms
+    done
+
+    # expose executables
+    for bb in ida assistant idat hvui; do
+      ln -s $IDADIR/$bb $out/bin/$bb
+    done
+
+    # runtimeDependencies don't get added to non-executables, and openssl is needed
+    #  for cloud decompilation
+    patchelf --add-needed libcrypto.so $IDADIR/libida.so
+    patchelf --add-needed libcrypto.so $IDADIR/libida32.so
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "The world's smartest and most feature-full disassembler";
+    homepage = "https://hex-rays.com/ida-pro/";
+    changelog = "https://hex-rays.com/products/ida/news/";
+    license = licenses.unfree;
+    mainProgram = "ida64";
+    maintainers = [ "misaka18931" ];
+    platforms = [ "x86_64-linux" ]; # Right now, the installation script only supports Linux.
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+  };
+}
