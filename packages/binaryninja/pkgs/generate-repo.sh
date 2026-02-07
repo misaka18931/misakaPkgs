bea893926534670fc898db10342a77e5e343bf82
miruku <miruku@miruku.moe>
2026-02-07 21:39:36 +0800

    test: binaryninja plugins CI

---
+++ packages/binaryninja/pkgs/generate-repo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env nix
+#! nix shell nixpkgs#nix-prefetch-github nixpkgs#nix-prefetch-scripts nixpkgs#parallel nixpkgs#jq nixpkgs#curl nixpkgs#coreutils --command bash
+
+# Usage: generate-repo.sh url/to/listing name
+
+curl -sL -o - "$1" | jq -c ".[]" | parallel -j 32 --pipe --will-cite --progress \
+ 'while read -r PLUGIN_JSON; do
+    echo $PLUGIN_JSON | jq --arg sha256 \
+                   "$(nix-prefetch-url 2>/dev/null \
+                                       --unpack \
+                                       --type sha256 $(echo $PLUGIN_JSON | jq -r ".packageUrl" ))" \
+                   ". + {sha256: \$sha256}"
+  done' \
+  | jq -s "." > "$2"
