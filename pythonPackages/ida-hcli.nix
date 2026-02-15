1f0a479d904ad181336d3a4a854bcf7ac6b78d4d
miruku <miruku@miruku.moe>
2026-02-15 16:49:25 +0800

    python3Packages.ida-hcli: 0.15.3 -> 0.15.13
    Diff: https://github.com/HexRaysSA/ida-hcli/compare/v0.15.3...v0.15.13

--- pythonPackages/ida-hcli.nix
+++ pythonPackages/ida-hcli.nix
@@ -25,13 +25,13 @@
 
 buildPythonPackage rec {
   pname = "ida-hcli";
-  version = "0.15.3";
+  version = "0.15.13";
 
   src = fetchFromGitHub {
     owner = "HexRaysSA";
     repo = "ida-hcli";
     rev = "v${version}";
-    hash = "sha256-lhdvV86ymWDBN1zrcymPCfopv7dN5gW9wrDg78K2F/w=";
+    hash = "sha256-afNLxdY9GUxQwTvL/PeiZb9vojTR9Wh7SdL0JciZFbM=";
   };
 
   pyproject = true;
