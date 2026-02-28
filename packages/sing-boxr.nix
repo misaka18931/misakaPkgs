a663ea185fd415b03cc294863c0cffd6712665e9
miruku <miruku@miruku.moe>
2026-03-01 00:44:32 +0800

    pkgs: add package sing-boxr

---
+++ packages/sing-boxr.nix
@@ -0,0 +1,14 @@
+pkgs: pkgs.sing-box.overrideAttrs (prev: {
+  version = "";
+  src = pkgs.fetchFromGitHub {
+    owner = "reF1nd";
+    repo = "sing-box";
+    rev = "6da7e538e122b77c226ce48c1a6b4ecb61fd3fc4";
+    hash = "sha256-lhkz/mXydZz5iJllqSp4skA4+jxs8oUmon/oFs98Zfc=";
+  };
+  vendorHash = "sha256-vVLaG0PV1OXA+YL67BnrHJiSkNVzJbZ8TeMKbO2rMu0=";
+  meta = prev.meta // {
+    homepage = "https://sing-boxr.dustinwin.us.kg";
+    description = prev.meta.description + " (fork at reF1nd/sing-box)";
+  };
+})
--- update.sh
+++ update.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env nix
 #! nix shell nixpkgs#nix-update --command bash
 
+#packages
+for path in packages/*; do
+            package=$(basename $path .nix)
+            [ "$package" = default ] && continue
+
+            case $package in
+                sing-boxr)
+                    version=branch=reF1nd-dev
+                    ;;
+                *)
+                    version=
+                    continue
+                    ;;
+            esac
+            if [ -n "$version" ]; then
+                version=--version=$version
+            fi
+            nix-update $package \
+                       --flake \
+                       --build \
+                       --commit \
+                       --override-filename=$path \
+                       $version
+         done
+
 # pythonPackages
 for path in pythonPackages/*; do
             package=$(basename $path .nix)
@@ -12,6 +37,9 @@ for path in pythonPackages/*; do
                     ;;
                 xdis-git)
                     version=branch
+                    ;;
+                *)
+                    version=
                     ;;
             esac
             if [ -n "$version" ]; then
