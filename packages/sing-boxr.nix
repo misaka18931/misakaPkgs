8c2d88e4389e1e892bf543c178737f48ab362e97
miruku <miruku@miruku.moe>
2026-03-01 00:57:09 +0800

    sing-boxr: 2.12 -> 1.13.0-unstable-2026-02-28
    Diff: https://github.com/reF1nd/sing-box/compare/6da7e538e122b77c226ce48c1a6b4ecb61fd3fc4...6da7e538e122b77c226ce48c1a6b4ecb61fd3fc4

--- packages/sing-boxr.nix
+++ packages/sing-boxr.nix
@@ -1,10 +1,10 @@
 pkgs: pkgs.sing-box.overrideAttrs (prev: {
-  version = "";
+  version = "1.13.0-unstable-2026-02-28";
   src = pkgs.fetchFromGitHub {
     owner = "reF1nd";
     repo = "sing-box";
-    rev = "6da7e538e122b77c226ce48c1a6b4ecb61fd3fc4";
-    hash = "sha256-lhkz/mXydZz5iJllqSp4skA4+jxs8oUmon/oFs98Zfc=";
+    rev = "7536a8302dafc0ce1e3bb15d4ee6cb2709d5af24";
+    hash = "sha256-so6OAF9oeaKlXndg3usod6CNAUwC0a9NX1TOS8MYG/Y=";
   };
   vendorHash = "sha256-vVLaG0PV1OXA+YL67BnrHJiSkNVzJbZ8TeMKbO2rMu0=";
   meta = prev.meta // {
